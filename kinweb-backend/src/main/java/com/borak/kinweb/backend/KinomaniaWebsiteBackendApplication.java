d87d3dfb7afece84944ef799202bf56034c8792a
ChocolateWizard <despotjev@gmail.com>
2023-07-10 18:59:37 +0200

    Initial commit

---
+++ kinweb-backend/src/main/java/com/borak/kinweb/backend/KinomaniaWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.borak.kinweb.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinomaniaWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinomaniaWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ kinweb-backend/src/test/java/com/borak/kinweb/backend/KinomaniaWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borak.kinweb.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinomaniaWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
